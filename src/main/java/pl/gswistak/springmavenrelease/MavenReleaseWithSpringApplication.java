f9e135515612faf637760df8b15b6838371033e7
grzegorzswistak <grzegorz.swistak2@gmail.com>
2021-05-13 11:34:46 +0200

    Repo url

--- src/main/java/pl/gswistak/springmavenrelease/MavenReleaseWithSpringApplication.java
+++ src/main/java/pl/gswistak/springmavenrelease/MavenReleaseWithSpringApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MavenReleaseWithSpringApplication {
 
+    //
     public static void main(String[] args) {
         SpringApplication.run(MavenReleaseWithSpringApplication.class, args);
     }
